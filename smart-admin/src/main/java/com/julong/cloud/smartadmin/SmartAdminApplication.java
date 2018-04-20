14724d68ea0c5e10300ea32c53ff18294f4d55af
qq57694878 <57694878@qq.com>
2018-04-20 17:20:25 +0800

    增加代码

---
+++ smart-admin/src/main/java/com/julong/cloud/smartadmin/SmartAdminApplication.java
@@ -0,0 +1,12 @@
+package com.julong.cloud.smartadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAdminApplication.class, args);
+	}
+}
--- smart-center/src/main/java/com/julong/cloud/smartcenter/SmartCenterApplication.java
+++ smart-center/src/main/java/com/julong/cloud/smartcenter/SmartCenterApplication.java
@@ -2,7 +2,9 @@ package com.julong.cloud.smartcenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SmartCenterApplication {
 
